01db227d590da6484da6fbf31fb74d80fefc7cdb
dyxmy49-dev <dyxmy49@gmail.com>
2026-06-13 06:37:01 +0300

    Add BloxStrike self-contained bundle

---
+++ bloxstrike.lua
@@ -0,0 +1,5 @@
+-- BloxStrike | 100% self-contained | No external URLs
+-- All 15 modules embedded, no HTTP needed
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/dyxmy49-dev/bs/main/bloxstrike.lua"))()
+
+local _M = {}
